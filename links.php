f93f9fcf041859a9561ee453ccc21731dd656e68
Kejie Shao <kjshao@dicp.ac.cn>
2016-11-18 16:59:27 +0800

    new
    new

--- links.php
+++ links.php
@@ -1,29 +1,29 @@
 <div class="container" style="width:100%; padding:15px 45px 15px 45px;">
-<table class='table table1' style="padding:0px;">
+<table class='table table1' style="padding:0px; table-layout:fixed;">
   <thead>
       <th>
       <div class="panel1 panel panel-default">
-        <center><div class="panel2b panel-body" style="width:102px; height:80px; font-size:18px; background-color:#ffffff">心理咨询</div></center>
+        <center><div class="panel2b panel-body" style="color:#fff; width:100%; height:80px; font-size:20px; background-color:#ffffff; background-image:url(img/b5.svg); border-radius:3px;">心理咨询</div></center>
       </div>
       </th>
       <th>
       <div class="panel1 panel panel-success">
-        <center><div class="panel2b panel-body panel-success" style="width:102px; height:80px; font-size:18px;">校友网</div></center>
+        <center><div class="panel2b panel-body" style="color:#fff; width:100%; height:80px; font-size:20px; background-image:url(img/b7.svg); border-radius:3px;">校友网</div></center>
       </div>
       </th>
       <th>
       <div class="panel1 panel panel-success">
-        <center><div class="panel2b panel-body panel-success" style="width:102px; height:80px; font-size:18px;">毕业生专题</div></center>
+        <center><div class="panel2b panel-body" style="color:#fff; width:100%; height:80px; font-size:20px; background-image:url(img/b4.svg); border-radius:3px;">毕业生专题</div></center>
       </div>
       </th>
       <th>
       <div class="panel1 panel panel-success">
-        <center><div class="panel2b panel-body panel-success" style="width:102px; height:80px; font-size:18px;">友情链接</div></center>
+        <center><div class="panel2b panel-body" style="color:#fff; width:100%; height:80px; font-size:20px; background-image:url(img/b9.svg); border-radius:3px;">友情链接</div></center>
       </div>
       </th>
       <th>
       <div class="panel1 panel panel-success">
-        <center><div class="panel2b panel-body panel-success" style="width:102px; height:80px; font-size:18px;">学习资源</div></center>
+        <center><div class="panel2b panel-body" style="color:#fff; width:100%; height:80px; font-size:20px; background-image:url(img/b1.svg); border-radius:3px;">学习资源</div></center>
       </div>
       </th>
   </thead>
