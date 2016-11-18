57d59cb3b2bf827d06697e4905566bcf9de4cb50
Kejie Shao <kjshao@dicp.ac.cn>
2016-11-19 02:25:48 +0800

    new
    new

--- jumbotron.php
+++ jumbotron.php
@@ -1,13 +1,7 @@
 <div class="jumbotron">
   <div class="row">
     <div class="col-xs-2 text-right">
-      <img src="dicp.svg" height="100">
-    </div>
-    <div class="col-xs-4 text-left">
-      <h2>中国科学院<br>大连化学物理研究所</h2>
-    </div>
-    <div class="col-xs-6 text-left">
-      <h2><br>研究生部</h2>
+      <img src="img/dicp1.svg" width="1201">
     </div>
   </div>
 </div>
