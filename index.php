50a625884422e8681dc8e619441f200e55437346
Kejie Shao <kjshao@dicp.ac.cn>
2016-11-19 16:27:48 +0800

    new
    new

--- index.php
+++ index.php
@@ -4,28 +4,27 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
-  <title>大连化物所研究生部主页</title>
+  <title>大连化物所研究生部</title>
   <meta name="author" content="Kejie Shao">
   <meta charset="utf-8">
   <link rel="stylesheet" href="../bootstrap/3.3.6/css/bootstrap.min.css">
-  <link href="css/drop.css" rel="stylesheet" type="text/css" />
+  <link href="css/basic.css" rel="stylesheet" type="text/css" />
+  <link href="css/carousel.css" rel="stylesheet" type="text/css" />
+  <link href="css/slider.css" rel="stylesheet" type="text/css" />
   <script src="../jquery/1.12.4/jquery.min.js"></script>
   <script src="../bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <script src="js/vticker.js"></script>
-  <script src="js/drop.js"></script>
-  <script type="text/javascript" src="js/jquery.nbspSlider.1.1.min.js"></script>
+  <script src="js/dropdown.js"></script>
   <script src="js/jssor.slider-21.1.6.mini.js" type="text/javascript"></script>
   <script src="js/init.js" type="text/javascript"></script>
-  <link href="css/carousel.css" rel="stylesheet" type="text/css" />
-  <link href="css/sliderx.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 <?php include_once('jumbotron.php'); ?>
 <?php include_once('nav.php'); ?>
-<?php include_once('sliderx.php'); ?>
+<?php include_once('slider.php'); ?>
 <?php include_once('news.php'); ?>
-<?php include_once('tab1.php'); ?>
+<?php include_once('infos.php'); ?>
 <?php include_once('students.php'); ?>
 <?php include_once('carousel.php'); ?>
 <?php include_once('links.php'); ?>
--- slider.php
+++ slider.php
@@ -1,77 +1,64 @@
-<!--幻灯图片开始！-->
-<div class="slider">
-  <div class="sub-slider">
-    <ul>
-    <!--幻灯片开始！-->
-    <li >
-      <div class="sub-slider-pic">
-      <!--图片大小：400px(W)*300px(H)-->
-        <img src="images/slider/pacman.jpg" alt="pic" />
-      </div>
-      <div class="sub-slider-text">
-        <p class="sub-slider-text-title">
-          Pacman Mechnism in Graphene Cutting
-        </p>
-        <p class="sub-slider-text-time">
-          Angew. Chem. Int. Ed. 2016, 55, 9918–9921.
-        </p>
-        <p class="sub-slider-text-summary">
-          A "Pac-Man" mechanism is revealed for the metal-nanoparticle-catalyzed cutting of graphene by employing a
-          multiscale simulation approach. Although the overall cutting rate is proportional to the surface area
-          of the nanoparticle, the rate-limiting step of graphene cutting is actually at the
-          graphene–metal interface. Such an apparent inconsistency turns out to be a result of the
-                        distinct etching behaviors at zigzag and armchair edge sites. 
-         </p>
-       </div>
-     </li>
-            <!--幻灯片结束！-->
-
-            <!--幻灯片开始！-->
-            <li >
-            	<div class="sub-slider-pic">
-            		<!--图片大小：400px(W)*300px(H)-->
-                    <img src="images/slider/icngm.jpg" alt="pic" />
-                </div>
-                <div class="sub-slider-text">
-            		<p class="sub-slider-text-title">
-                    	ICNGM 2015
-                    </p>
-                    <p class="sub-slider-text-time">
-                    	Hefei, China. Dec 10-12, 2015.
-                    </p>
-                    <p class="sub-slider-text-summary">
-                    	The first international conference on nucleation and growth mechanisms 
-                        (<a href="http://icngm.ustc.edu.cn/">ICNGM 2015</a>) has been successfully
-                        held at Hefei on Dec 10-12, 2015. More than 30 invited talks have been presented 
-                        in ICNGM 2015. Prof. Zhenyu Li is the conference chair, and all graduate
-                        students in our group are proud of serving as conference volunteers.
-                    </p>
-                </div>
-            </li>
-            <!--幻灯片结束！-->
-
-           <!--幻灯片开始！-->
-           <li style="margin-left:40px;">
-            	<div class="sub-slider-pic">
-            		<!--图片大小：400px(W)*300px(H)-->
-                    <img src="images/slider/dimer.jpg" alt="pic" />
-                </div>
-                <div class="sub-slider-text">
-            		<p class="sub-slider-text-title">
-                    	Carbon Dimer in Graphene Growth
-                    </p>
-                    <p class="sub-slider-text-time">
-                    	Phys. Rev. Lett. 2015, 114, 216102.
-                    </p>
-                    <p class="sub-slider-text-summary">
-                    	Cu substrates are highly preferred for the potential mass production of high-quality graphene. Using
-                        multiscale modeling, we identify C-C dimers as the dominant feeding species in the epitaxial growth of
-                        graphene on both Cu(111) and Cu(100) substrates, which explains why graphene growth is diffusion limited 
-                        on Cu(111), but attachment limited on Cu(100). 
-                    </p>
-                </div>
-            </li>
-            <!--幻灯片结束！-->
-        </ul>
+<div class="container sliderDiv">
+<div id="jssor_2" class="slider">
+<div data-u="slides" style="cursor: default; border:0px; position: relative; top: 0px; left: 0px; width: 1200px; height: 300px; overflow: hidden;">
+<!-- Items -->
+  <div data-p="150.00">
+    <img src="img/S01.jpg" style="position: absolute; top: 0px; left: 0px; width: 600px; height: 300px;" />
+    <div style="position: absolute; top: 0px; left: 601px; width: 600px; height: 300px; background-color: rgba(173,173,173,0.3); font-size: 20px; color: #212121; line-height: 30px; text-align: center;">
+      <p>
+      test test
+      </p>
     </div>
+  </div>
+<!-- Items -->
+  <div data-p="150.00">
+    <img src="img/S02.jpg" style="position: absolute; top: 0px; left: 0px; width: 600px; height: 300px;" />
+    <div style="position: absolute; top: 0px; left: 601px; width: 600px; height: 300px; background-color: rgba(173,173,173,0.3); font-size: 20px; color: #212121; line-height: 30px; text-align: center;">
+      <p>
+      test test
+      </p>
+    </div>
+  </div>
+<!-- Items -->
+  <div data-p="150.00">
+    <img src="img/S03.jpg" style="position: absolute; top: 0px; left: 0px; width: 600px; height: 300px;" />
+    <div style="position: absolute; top: 0px; left: 601px; width: 600px; height: 300px; background-color: rgba(173,173,173,0.3); font-size: 20px; color: #212121; line-height: 30px; text-align: center;">
+      <p>
+      test test
+      </p>
+    </div>
+  </div>
+<!-- Items -->
+  <div data-p="150.00">
+    <img src="img/S04.jpg" style="position: absolute; top: 0px; left: 0px; width: 600px; height: 300px;" />
+    <div style="position: absolute; top: 0px; left: 601px; width: 600px; height: 300px; background-color: rgba(173,173,173,0.3); font-size: 20px; color: #212121; line-height: 30px; text-align: center;">
+      <p>
+      test test
+      </p>
+    </div>
+  </div>
+<!-- Items -->
+  <div data-p="150.00">
+    <img src="img/S05.jpg" style="position: absolute; top: 0px; left: 0px; width: 600px; height: 300px;" />
+    <div style="position: absolute; top: 0px; left: 601px; width: 600px; height: 300px; background-color: rgba(173,173,173,0.3); font-size: 20px; color: #212121; line-height: 30px; text-align: center;">
+      <p>
+      test test
+      </p>
+    </div>
+  </div>
+<!-- Items -->
+  <!-- Bullet Navigator -->
+  <div data-u="navigator" class="jssorb03" style="bottom:16px;right:16px;">
+      <!-- bullet navigator item prototype -->
+      <div data-u="prototype" style="width:21px;height:21px;">
+          <div data-u="numbertemplate"></div>
+      </div>
+  </div>
 </div>
+  <!-- Arrow Navigator -->
+  <span data-u="arrowleft" class="jssora22l" style="top:0px;left:40px;width:40px;height:58px;" data-autocenter="2"></span>
+  <span data-u="arrowright" class="jssora22r" style="top:0px;right:40px;width:40px;height:58px;" data-autocenter="2"></span>
+</div>
+</div>
+<script type="text/javascript">jssor_2_slider_init();</script>
+<!-- #endregion Jssor Slider End -->
