d235268969d6e76bc4845677c63e6a9dfccd4b85
Kejie Shao <kjshao@dicp.ac.cn>
2016-11-21 15:10:02 +0800

    new
    new

--- slider.php
+++ slider.php
@@ -4,27 +4,47 @@
 <!-- Items -->
   <div data-p="150.00">
     <img src="img/S01.jpg" style="position: absolute; top: 0px; left: 0px; width: 1200px; height: 400px;" />
-    <!--<div style="position: absolute; top: 200px; left: 300px; width: 600px; height: 200px; background-color: rgba(173,173,173,0.3); font-size: 20px; color: #212121; line-height: 30px; text-align: center;">
+    <div style="position: absolute; padding:17px 25px 10px 25px; top: 300px; left:100px; width: auto; height: auto; background-color: rgba(0,0,0,0.50); font-size: 24px; color: #fff; line-height: 30px; text-align: center;">
       <p>
-      test test
+      我所石墨烯基柔性化、微型化超级电容器研究取得新进展
       </p>
-    </div>-->
+    </div>
   </div>
 <!-- Items -->
   <div data-p="150.00">
     <img src="img/S02.jpg" style="position: absolute; top: 0px; left: 0px; width: 1200px; height: 400px;" />
+    <div style="position: absolute; padding:17px 25px 10px 25px; top: 300px; left:100px; width: auto; height: auto; background-color: rgba(0,0,0,0.50); font-size: 24px; color: #fff; line-height: 30px; text-align: center;">
+      <p>
+      青海盐湖 MTO 装置首次投料试车取得圆满成功
+      </p>
+    </div>
   </div>
 <!-- Items -->
   <div data-p="150.00">
     <img src="img/S03.jpg" style="position: absolute; top: 0px; left: 0px; width: 1200px; height: 400px;" />
+    <div style="position: absolute; padding:17px 25px 10px 25px; top: 300px; left:100px; width: auto; height: auto; background-color: rgba(0,0,0,0.50); font-size: 24px; color: #fff; line-height: 30px; text-align: center;">
+      <p>
+      我所光催化辅助燃料电池 ORR 研究取得新进展
+      </p>
+    </div>
   </div>
 <!-- Items -->
   <div data-p="150.00">
     <img src="img/S04.jpg" style="position: absolute; top: 0px; left: 0px; width: 1200px; height: 400px;" />
+    <div style="position: absolute; padding:17px 25px 10px 25px; top: 300px; left:100px; width: auto; height: auto; background-color: rgba(0,0,0,0.50); font-size: 24px; color: #fff; line-height: 30px; text-align: center;">
+      <p>
+      张涛荣获 2016 年何梁何利科学与技术进步化学奖
+      </p>
+    </div>
   </div>
 <!-- Items -->
   <div data-p="150.00">
     <img src="img/S05.jpg" style="position: absolute; top: 0px; left: 0px; width: 1200px; height: 400px;" />
+    <div style="position: absolute; padding:17px 25px 10px 25px; top: 300px; left:100px; width: auto; height: auto; background-color: rgba(0,0,0,0.50); font-size: 24px; color: #fff; line-height: 30px; text-align: center;">
+      <p>
+      大连相干光源首次出光
+      </p>
+    </div>
   </div>
 <!-- Items -->
   <!-- Bullet Navigator -->
