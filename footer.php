0faa75684ff6b39dc9171ba417cbf1dad00f27bf
Kejie Shao <kjshao@dicp.ac.cn>
2016-11-23 22:14:02 +0800

    new
    new

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<center style="border-bottom-left-radius:10px; border-bottom-right-radius:10px;  padding:35px 0px 20px 0px; background-color: #3c4486; color:#d7d7d7">
-  <p>CopyRight 1999-2016. Dalian Institute of Chemical Physics, Chinese Academy of Sciences. All Rights Reserved.</p>
-  <p>中国科学院大连化学物理研究所 版权所有 辽 ICP 备 05000861 号</p>
-</center>
+<p style="border-bottom-left-radius:10px; border-bottom-right-radius:10px;  padding:35px 0px 20px 0px; background-color: #3c4486; color:#d7d7d7; text-align:center; width:1200px">
+CopyRight 1999-2016. Dalian Institute of Chemical Physics, Chinese Academy of Sciences. All Rights Reserved.
+<br><br>中国科学院大连化学物理研究所 版权所有 辽 ICP 备 05000861 号
+</p>
